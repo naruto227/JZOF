cbe7f2f589ea4bf0d9f05bfaad2bcc4a2ba1dd02
hzq <huangzhiqiang@dataguiding.com>
2016-12-23 11:17:44 +0800

    new

--- src/supplment_examples/Main.java
+++ src/supplment_examples/Main.java
@@ -17,8 +17,8 @@ public class Main {
             System.out.println("a = " + a + ", b = " + b);
 //            System.out.println("最大公约数：" + solution(a, b));
             System.out.println("最大公约数：" + max_yue.gcd(a, b));
-            System.out.println("最小公倍数：" + min_bei.bianli(a, b));
-
+            System.out.println("遍历求最小公倍数：" + min_bei.bianli(a, b));
+            System.out.println("利用a*b/(a与b的最大公约数)：" + a * b / max_yue.solution(a, b));
         }
     }
 }
