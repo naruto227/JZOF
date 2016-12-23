b2752b8196cb5ed943f41a82e71458710c84401d
hzq <huangzhiqiang@dataguiding.com>
2016-12-23 10:05:44 +0800

    最大

---
+++ src/com/company/两个栈实现一个队列.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by hzq on 16-12-18.
+ */
+public class 两个栈实现一个队列 {
+
+}
---
+++ src/supplment_examples/最大公约数.java
@@ -0,0 +1,17 @@
+package supplment_examples;
+
+import java.util.Scanner;
+
+/**
+ * Created by hzq on 16-12-23.
+ */
+public class 最大公约数 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("a = " + a + ", b = " + b);
+        }
+    }
+}
