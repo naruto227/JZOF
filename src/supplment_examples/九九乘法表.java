6cc80e5ce4d114ca5d819c4fd22662e7577b6e67
hzq <huangzhiqiang@dataguiding.com>
2016-12-24 10:19:19 +0800

    9*9

--- src/supplment_examples/九九乘法表.java
+++ src/supplment_examples/九九乘法表.java
@@ -9,21 +9,36 @@ import java.util.Scanner;
 public class 九九乘法表 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextInt()){
+        while (scanner.hasNextInt()) {
             int num = scanner.nextInt();
             solution(num);
+            solution1(num);
         }
     }
-//正三角
-    public static void solution(int num){
+
+    //正三角
+    public static void solution(int num) {
         for (int i = 1; i <= num; i++) {
             for (int j = 1; j <= num; j++) {
                 System.out.printf(j + "*" + i + "=" + i * j + "\t");
-                if(i == j){
+                if (i == j) {
                     System.out.println();
                     break;
                 }
             }
+        }
+    }
+
+    public static void solution1(int num) {
+        for (int row = 1; row <= num; row++) {       //循环行
+            for (int col = 1; col <= row; col++) {   //循环列
+                System.out.print(row * col);  //输出数值
+                if (row * col < 10) {     //一位数
+                    System.out.print(' ');
+                }
+                System.out.print(' ');        //输出数字之间的间隔空格
+            }
+            System.out.println();           //一行输出结束，换行
         }
     }
 }
