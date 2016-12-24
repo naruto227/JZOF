bfd6f4467b1115c83c79b842f0371a6f439b30d5
hzq <huangzhiqiang@dataguiding.com>
2016-12-24 23:25:52 +0800

    打印星

--- src/supplment_examples/打印星.java
+++ src/supplment_examples/打印星.java
@@ -18,7 +18,7 @@ public class 打印星 {
         int top = num + 1;
 //      打印前num行的星
         for (int i = 1; i < top; i++) {
-            for (int j = 0; j < top - i; j++) {
+            for (int j = 1; j < top - i; j++) {
                 System.out.printf(" ");
             }
             for (int j = 0; j < 2 * i - 1; j++) {
@@ -28,9 +28,11 @@ public class 打印星 {
         }
 //      打印num+1到2*num-1行的星
         for (int i = num + 1; i < 2 * num + 1; i++) {
-            for (int j = 0; j <= (i - num); j++) {
+//          打印空格数
+            for (int j = 1; j <= (i - num); j++) {
                 System.out.printf(" ");
             }
+//          打印星的数量
             for (int j = 0; j < (2 * num - 1 - 2 * (i - num)); j++) {
                 System.out.printf("*");
             }
