765abf73a34ceb41b3f1393bf2fb724c99398cd9
Michael Allan <huangzq30@163.com>
2016-12-26 18:07:44 +0800

    斐波那契数列

--- src/supplment_examples/斐波那契.java
+++ src/supplment_examples/斐波那契.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 /**
  * Created by hzq on 16-12-26.
  */
-/*f(n) = f(n-1) + f(n-2)*/
+/*f(n) = f(n-1) + f(n-2)
+* 1 1 2 3 5 8 13 21 34*/
 public class 斐波那契 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
