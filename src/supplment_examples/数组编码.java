df12a03290872c4699adf6bbf57a98a8355a91bb
Michael Allan <huangzq30@163.com>
2016-12-28 00:20:13 +0800

    arr_b

--- src/supplment_examples/数组编码.java
+++ src/supplment_examples/数组编码.java
@@ -1,5 +1,6 @@
 package supplment_examples;
 //引入array_list包下的Array类
+
 import array_list.Array;
 
 import java.util.Scanner;
@@ -7,14 +8,16 @@ import java.util.Scanner;
 /**
  * Created by hzq on 16-12-27.
  */
-/*要求：设有一数组A，长度是N，内部的数据是0到N-1之间的所有数字，例如当N等于5时，数组为：A={0,3,2,1,4}。针对A数组，有一个对应的编码数组B，B的长度和A的长度相等，规定数组B中元素的值规定如下：
+/*
+要求：设有一数组A，长度是N，内部的数据是0到N-1之间的所有数字，例如当N等于5时，数组为：A={0,3,2,1,4}。针对A数组，有一个对应的编码数组B，B的长度和A的长度相等，规定数组B中元素的值规定如下：
 a、 B[0]的值为0
 b、 B[i]的值是A数组中A[i]以前的值中比A[i]小的元素的个数。
-c、 例如示例中A数组{0,3,2,1,4}对应的编码数组B的值为{0,1,1,1,4}。*/
+c、 例如示例中A数组{0,3,2,1,4}对应的编码数组B的值为{0,1,1,1,4}
+*/
 public class 数组编码 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextInt()){
+        while (scanner.hasNextInt()) {
             int n = scanner.nextInt();//要生成的数组元素个数
             int min = scanner.nextInt();//数组值的范围[min,max]
             int max = scanner.nextInt();
@@ -23,7 +26,7 @@ public class 数组编码 {
         }
     }
 
-    public static void createaArrB(int[] arr){
+    public static void createaArrB(int[] arr) {
         int[] arr_b = new int[arr.length];
     }
 
