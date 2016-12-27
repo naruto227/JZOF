0b5fc9b61c8675f8fb46e9a8f08621410a58566e
Michael Allan <huangzq30@163.com>
2016-12-27 22:11:07 +0800

    数字统计

--- src/supplment_examples/数字统计.java
+++ src/supplment_examples/数字统计.java
@@ -16,20 +16,22 @@ public class 数字统计 {
     }
 
     public static int mostNum(int num) {
-        int[] ints = new int[32];
-        int n = 0;
+        int[] ints = new int[32];//存放拆分后的数字
+        int n = 0;//num数字的个数
         while (num != 0) {
-            ints[n] = num % 10;
-            n++;
-            num /= 10;
+            ints[n] = num % 10;//获取个位数
+            n++;//num数字的个数加1
+            num /= 10;//去掉个位数，原十位数变为个位数
         }
 
         int[] count = new int[10];
+        //统计0-9出现的次数
         for (int i = 0; i < n; i++) {
             count[ints[i]]++;
         }
 
         int index = 0, len1 = count.length;
+//        获得最大值的下标
         for (int i = 0; i < len1; i++) {
             if (count[index] <= count[i]) {
                 index = i;
