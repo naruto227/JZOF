4e40c3587b5440e987b5f450f37a29e044993e80
Michael Allan <huangzq30@163.com>
2016-12-31 01:14:10 +0800

    bubble sort

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -16,26 +16,26 @@ public class BubbleSort {
         while (scanner.hasNext()) {
             int n = scanner.nextInt();
             int[] ints = array.initialization(n);
-            sort2(ints, n);
+            sort1(ints, n);
         }
     }
 
     //固定首位为最小
     public static void sort1(int[] arr, int n) {
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             for (int j = i; j < n; j++) {
                 if (arr[i] > arr[j]) {
                     swap(arr, i, j);
                 }
             }
-            System.out.printf("第" + i + "步排序后的序列为：");
+            System.out.printf("第" + (i + 1) + "步排序后的序列为：");
             print_arr(arr);
         }
     }
 
     //序列两两比较
     public static void sort2(int[] arr, int n) {
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
