b5397e858add4f7885e94356371b306256300e6b
Michael Allan <huangzq30@163.com>
2017-01-03 13:26:07 +0800

    杨辉三角

--- src/supplment_examples/杨辉三角.java
+++ src/supplment_examples/杨辉三角.java
@@ -7,13 +7,25 @@ import java.util.Scanner;
  */
 public class 杨辉三角 {
     public static void main(String[] args) {
+        拉丁方阵 ld = new 拉丁方阵();
         Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextInt()){
+        while (scanner.hasNextInt()) {
             int n = scanner.nextInt();
+            ld.printMultiArr(createYHTriangle(n));
         }
     }
 
-    public static void createYHTriangle(int n){
-
+    public static int[][] createYHTriangle(int n) {
+        int[][] ints = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            ints[i][0] = 1;
+            ints[i][i] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < i; j++) {
+                ints[i][j] = ints[i - 1][j] + ints[i - 1][j - 1];
+            }
+        }
+        return ints;
     }
 }
