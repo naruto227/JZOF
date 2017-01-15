ddc55978338196d2bf16611708d91c198fece83f
Michael Allan <huangzq30@163.com>
2017-01-15 22:32:44 +0800

    quick sort

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -2,6 +2,7 @@ package sort;
 
 import array_list.Array;
 
+import static sort.BubbleSort.swap;
 import static supplment_examples.数组编码.print_arr;
 
 /**
@@ -15,4 +16,17 @@ public class QuickSort {
         print_arr(ints);
     }
 
+    private static int partition(int[] arr, int low, int high) {
+        int temp = arr[low];
+        while (low < high) {
+            while (low< high && arr[low]<= arr[high]){
+                --high;
+            }
+            swap(arr,low,high);
+            while (low<high && arr[low]>=arr[high]){
+                ++low;
+            }
+
+        }
+    }
 }
