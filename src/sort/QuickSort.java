f9fb94470de320fd10c8935d4318c518e9688175
Michael Allan <huangzq30@163.com>
2017-01-15 22:45:16 +0800

    1

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -6,6 +6,10 @@ import static sort.BubbleSort.swap;
 import static supplment_examples.数组编码.print_arr;
 
 /**
+ * 1）选择一个基准元素,通常选择第一个元素或者最后一个元素,
+ * 2）通过一趟排序讲待排序的记录分割成独立的两部分，其中一部分记录的元素值均比基准元素值小。另一部分记录的 元素值比基准值大。
+ * 3）此时基准元素在其排好序后的正确位置
+ * 4）然后分别对这两部分记录用同样的方法继续进行排序，直到整个序列有序。
  * Created by hzq on 17-1-14.
  */
 public class QuickSort {
@@ -18,9 +22,9 @@ public class QuickSort {
     }
 
     private static int partition(int[] arr, int low, int high) {
-        int temp = arr[low];
-        while (low < high) {
-            while (low < high && temp <= arr[high]) {
+        int temp = arr[low];//基准元素
+        while (low < high) {//从表的两端交替地向中间扫描
+            while (low < high && temp <= arr[high]) {//从high 所指位置向前搜索，至多到low+1 位置。将比基准元素小的交换到低端
                 --high;
             }
             swap(arr, low, high);
@@ -35,9 +39,9 @@ public class QuickSort {
 
     private static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
-            int index = partition(arr, low, high);
-            quickSort(arr, low, index - 1);
-            quickSort(arr, index + 1, high);
+            int index = partition(arr, low, high);//将表一分为二
+            quickSort(arr, low, index - 1);//递归对低子表递归排序
+            quickSort(arr, index + 1, high);//递归对高子表递归排序
         }
     }
 }
