9ad7ee8989d29c37870ec5bc95a7b47297678476
Michael Allan <huangzq30@163.com>
2017-01-22 16:54:56 +0800

    tesy

---
+++ src/sort/MergeSort.java
@@ -0,0 +1,28 @@
+package sort;
+
+import array_list.Array;
+
+import java.lang.annotation.ElementType;
+import java.util.Scanner;
+
+import static array_list.输出数列.print_arr;
+
+/**
+ * Created by Michael Allan on 2017/1/22.
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Array array = new Array();
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            int[] ints = array.initialization(n);
+            System.out.printf("原始序列为：");
+            print_arr(ints);
+        }
+    }
+
+    private static void MergeSort(){
+
+    }
+}
